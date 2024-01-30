bfcf88544110211be16b9091ea8cca43846bb979
Hung Vu <hungtienvu@gmail.com>
2024-01-30 15:57:00 +0700

    remove redux persist

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,21 @@ import { Provider } from "react-redux";
 import store from "./lib/store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { persistStore } from "redux-persist";
 
-import { PersistGate } from "redux-persist/integration/react";
 import { HelmetProvider } from "react-helmet-async";
 
 function App() {
-  let persistor = persistStore(store);
-
   return (
     <HelmetProvider>
       <Provider store={store}>
-        <PersistGate persistor={persistor} loading={null}>
-          <Grommet theme={myTheme}>
-            <main className="bg-brand">
-              <Header />
-              <Outlet />
-              <Footer />
-            </main>
-            <ToastContainer />
-          </Grommet>
-        </PersistGate>
+        <Grommet theme={myTheme}>
+          <main className="bg-brand">
+            <Header />
+            <Outlet />
+            <Footer />
+          </main>
+          <ToastContainer />
+        </Grommet>
       </Provider>
     </HelmetProvider>
   );
